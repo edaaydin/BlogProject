a55f5334640e4834b38a155ad26111fb76d84154
sedanurasln <sedanur.aslan121@gmail.com>
2025-01-28 13:38:59 +0300

    Category.cs ve CategoryController.cs sayfaları eklendi.

---
+++ BlogProject/Controllers/CategoryController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MVC_BlogProject.Models.Context;
+using MVC_BlogProject.Models.Entities;
+using MVC_BlogProject.Models.Repositories.Abstract;
+using MVC_BlogProject.Models.Repositories.Concrete;
+
+public class CategoryController : Controller
+{
+    private readonly ICategoryRepo _categoryRepository;
+
+
+    public CategoryController(ICategoryRepo categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+    public IActionResult Index()
+    {
+        var categories = _categoryRepository.GetList();
+        return View(categories);
+    }
+
+    [HttpGet]
+    public IActionResult CategoryAdd()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult CategoryAdd(Category p)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View("CategoryAdd");
+        }
+        _categoryRepository.Insert(p);
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult CategoryDelete(int id)
+    {
+        var category = _categoryRepository.GetById(id);
+        if (category != null)
+        {
+            _categoryRepository.Delete(category);
+        }
+        return RedirectToAction("Index");
+    }
+
+    public IActionResult CategoryUpdate(Category p)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(p); // Formu hata mesajı ile tekrar göster
+        }
+
+        var x = _categoryRepository.GetById(p.Id);
+        if (x == null)
+        {
+            return NotFound();
+        }
+
+        if (string.IsNullOrWhiteSpace(p.Name))
+        {
+            ModelState.AddModelError("Name", "Kategori adı boş bırakılamaz!");
+            return View(p);
+        }
+
+        x.Name = p.Name;
+        _categoryRepository.Update(x);
+        return RedirectToAction("Index");
+    }
+
+
+}
+
--- BlogProject/Models/Entities/Category.cs
+++ BlogProject/Models/Entities/Category.cs
@@ -1,9 +1,18 @@
-﻿namespace MVC_BlogProject.Models.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace MVC_BlogProject.Models.Entities
 {
     public class Category
     {
+        public Category()
+        {
+            Articles = new List<Article>(); // Navigation property için liste başlatılıyor
+        }
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
+
         public List<Article> Articles { get; set; } // bir kategorinin birden fazla makalesi olabilir.
     }
 }
